d734374af5e21c1df5ef8f5541bc4967c1738266
Ben Sindberg <sindbergb@uwplatt.edu>
2025-11-26 16:25:22 -0600

    upload to github

--- FinalProjectOS/src/PCBState.java
+++ FinalProjectOS/src/PCBState.java
@@ -1,5 +1,8 @@
 public enum PCBState {
+    //PCB is ready to get running, able to be scheduled
     READY,
+    //PCB is actively running, using the CPU
     RUNNING,
+    //Something is stopping this PCB from being ready!
     BLOCKED;
 }
